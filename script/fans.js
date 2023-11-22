61c11974f8cf822d84998a06e088547ba8d22a78
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2023-11-22 13:12:22 -0300

    Add files via upload

---
+++ script/fans.js
@@ -0,0 +1,21 @@
+
+const inputs = document.querySelectorAll(".formulario__input");
+const formulario = document.getElementById("form");
+
+function activarFormLabel(){
+    inputs.forEach(input =>{
+        input.addEventListener("focus",()=>{
+            const label = document.querySelector(`label[for=${input.id}]`);
+            label.classList.add("formulario__label_active");
+        });
+
+        input.addEventListener("blur",()=>{
+            if(input.value.trim()==""){
+            const label = document.querySelector(`label[for=${input.id}]`);
+            label.classList.remove("formulario__label_active");
+            }
+        })
+    })
+}
+
+formulario.addEventListener("mousedown", activarFormLabel);
